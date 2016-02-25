31a84200910e20edd4e6122e66b9b135ab0551a6
Jose Orlando Vargas Sanchez <masterjovs@hotmail.com>
2016-02-25 13:37:07 -0500

    ingresado código de miniReto

--- miniReto_101/ViewController.swift
+++ miniReto_101/ViewController.swift
@@ -9,10 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+ 
+    @IBAction func iniciaReto(sender: AnyObject) {
+        
+        
+        var par_o_impar : Int
+        var divisible_5 : Int
+        
+        for var i = 0 ; i < 101 ; i++ {
+            //print(i)
+            
+            par_o_impar = i % 2
+            divisible_5 = i % 5
+            
+            
+            switch par_o_impar {
+                
+            case 0 :
+                if i == 0 {
+                    print("\(i)\tNumero Cero - Nulo - NO PAR / IMPAR")
+                }
+                else if i != 0 {
+                    print("\(i)\t#par")
+                    
+                }
+                
+            default:
+                print("\(i)\t#Impar")
+                
+            }
+            if i < 41 && i > 29 {
+                print("\(i)\t#Viva Swift!!!")
+            }
+            
+            if divisible_5 == 0 && i != 0{
+                print("\(i)\t#Bingo!!!")
+            }
+            
+        }
+
+    
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Hola
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +66,7 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
 }
 
